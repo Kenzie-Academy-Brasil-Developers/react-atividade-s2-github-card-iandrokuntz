ced0b352baba024a04a33c61ce472f30412df8c8
iandrokuntz <ianbertoluchi99@gmail.com>
2021-11-17 13:42:14 -0300

    atividade pronta

--- react-atividade-s2-github-card-iandrokuntz.git/src/components/Card/index.js
+++ react-atividade-s2-github-card-iandrokuntz.git/src/components/Card/index.js
@@ -14,6 +14,7 @@ const Card = ({api, setApi}) => {
         <div>
             {api.map((item,index) => (
                 <div key={index}>
+                    <img src={item.data.owner.avatar_url} alt="user-profile" width="130px" height="130px"/>
                     <h2>{item.data.full_name}</h2>
                     <p>{item.data.description}</p>
                     <button onClick={() => handleClean(item)}>Clean repositories</button>
